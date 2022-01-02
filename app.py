4483feb07c5723ee0e908c7e1b568d2b176f82f8
Charlie <47061354+chgagnon@users.noreply.github.com>
2022-01-02 13:46:11 -0500

    send database state to slack

--- app.py
+++ app.py
@@ -20,6 +20,10 @@ KILL_PREFIX_LEN = len(KILL_CMD_PREFIX)
 KILL_COMMAND = KILL_CMD_PREFIX + USRNAME_PATTERN
 
 MAIN_CHANNEL_NAME = "main_chat"
+VOTE_RESULTS_CHANNEL_NAME = "vote-results"
+
+PRAYER_STR = 'prayer'
+KILL_STR = 'kill'
 
 def test_database_connection():
     """ Connect to the PostgreSQL database server """
@@ -52,13 +56,12 @@ def test_database_connection():
 def cast_vote_to_database(voter_id, target_name, vote_type):
     _, voter_name = translate_user_id_to_name(voter_id)
 
-
     # convert from Python enum to PostGres enum
     if vote_type == VoteType.KILL:
-        vote_type = 'kill'
+        vote_type = KILL_STR
     else:
         assert(vote_type == VoteType.PRAYER)
-        vote_type = 'prayer'
+        vote_type = PRAYER_STR
 
     """ insert a new vote into the votes table """
     sql = """INSERT INTO votes(target_name, voter_name, vote_type)
@@ -86,6 +89,35 @@ def cast_vote_to_database(voter_id, target_name, vote_type):
         if conn is not None:
             conn.close()
 
+def send_database_state_to_slack():
+    slack_msg = "VOTE TALLY UPDATE:\n"
+
+    for vote_type in [PRAYER_STR, KILL_STR]:
+        slack_msg += f"for vote type: {vote_type}:\n"
+        conn = None
+        try:
+            """ query data from the votes table """
+            conn = psycopg2.connect(os.environ["DATABASE_URL"])
+            cur = conn.cursor()
+            cur.execute(f"SELECT target_name, COUNT(*) FROM votes WHERE vote_type = {vote_type} ORDER BY target_name")
+            print("The number of targets: ", cur.rowcount)
+            row = cur.fetchone()
+
+            while row is not None:
+                target_str = f"TARGET: {row[0]}"
+                numvotes_str = f"| VOTES: {row[1]}"
+                slack_msg += target_str.ljust(85) + numvotes_str.rjust(14) + "\n"
+                row = cur.fetchone()
+
+            cur.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
+    
+    app.client.chat_postMessage(channel=VOTE_RESULTS_CHANNEL_NAME, text=slack_msg)
+
 # Initializes your app with your bot token and socket mode handler
 app = App(token=os.environ.get('SLACK_BOT_TOKEN'), signing_secret=os.environ.get("SLACK_SIGNING_SECRET"))
 
@@ -94,7 +126,9 @@ def action_button_click(event, say):
     say("dude what do you want")
 
 def translate_user_id_to_name(user_id):
-    users_list = app.client.users_list(token=os.environ.get("SLACK_BOT_TOKEN"))
+    # token should be passed automatically
+    # (specified token when initializing app)
+    users_list = app.client.users_list()
     for user in users_list['members']:
         if user['id'] == user_id:
             return (user['id'], user['real_name'])
@@ -153,6 +187,7 @@ def update_kill_vote(voting_player, target_player):
     # update_vote_assignments(voting_player, target_player, VoteType.KILL) 
     # show_vote_results()
     cast_vote_to_database(voting_player, target_player, VoteType.KILL)
+    send_database_state_to_slack()
 
 @app.command("/kill")
 def handle_kill_vote(ack, respond, command):
@@ -176,6 +211,7 @@ def update_prayer(praying_player, prayer_target):
     # update_vote_assignments(praying_player, prayer_target, VoteType.PRAYER)
     # show_vote_results()
     cast_vote_to_database(praying_player, prayer_target, VoteType.PRAYER)
+    send_database_state_to_slack()
 
 @app.command("/prayto")
 def handle_prayer(ack, respond, command):
